cdd2650d5c7eeaf0d4dfbfffe729cc5eb54565d8
yamamotoyuri <y.yamamoto.cingroup2012@gmail.com>
2023-08-06 19:27:45 +0900

    php基礎

---
+++ 03.繰り返し処理/lesson1.php
@@ -0,0 +1,13 @@
+﻿<?php
+// 配列に「日本,アメリカ,イギリス,フランス」を格納し、foreach文を使って順番に下記のフォーマットで出力して下さい。
+// 要素番号:国名
+
+
+$countrylist = array('日本','アメリカ','イギリス','フランス');
+
+foreach($countrylist as $country){
+    echo '要素番号：'.$country.'<br/>';git
+    
+}
+
+?>
