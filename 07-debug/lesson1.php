93a911226d7bff4381ba16ba94fbb0b966ad3d1e
yamamotoyuri <y.yamamoto.cingroup2012@gmail.com>
2023-08-17 17:57:52 +0900

    PHP/デバック

--- 07-debug/lesson1.php
+++ 07-debug/lesson1.php
@@ -20,7 +20,7 @@ if (!empty($_POST)) {
 </head>
 <body>
     <section>
-    <form action='./lesson.php' method="post">
+    <form action='./lesson1.php' method="post">
         <label>姓</label>
         <input type="text" name="last_name"/>
         <label>名</label>
